241764db7054d099cd7fb3e2f073675bd7d4c5eb
MirnaCe <mjimenezcanseco@gmail.com>
2017-12-03 12:30:08 -0600

    Interfaz de notificaciones

---
+++ app/src/main/java/edu/unsis/recetario/notifications/NotificationToma.java
@@ -0,0 +1,15 @@
+package edu.unsis.recetario.notifications;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import edu.unsis.recetario.R;
+
+public class NotificationToma extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_notification_toma);
+    }
+}
