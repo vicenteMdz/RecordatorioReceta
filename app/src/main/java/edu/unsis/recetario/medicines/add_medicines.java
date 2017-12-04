9392010c2da207862f1a244c8904c4919fc5a921
Marifer <rsmaribel93>
2017-12-03 19:35:08 -0600

    validacion de caompos

--- app/src/main/java/edu/unsis/recetario/medicines/add_medicines.java
+++ app/src/main/java/edu/unsis/recetario/medicines/add_medicines.java
@@ -4,12 +4,16 @@ import android.app.TimePickerDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import edu.unsis.recetario.R;
+import edu.unsis.recetario.medicines.model.Medicamento;
+
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Locale;
 import android.app.DatePickerDialog;
 import android.app.DatePickerDialog.OnDateSetListener;
 import android.text.InputType;
+import android.text.TextUtils;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
@@ -23,11 +27,11 @@ import android.widget.TextView;
 import android.widget.TimePicker;
 import android.widget.Toast;
 
-public class add_medicines extends AppCompatActivity implements OnClickListener {
+public class add_medicines extends AppCompatActivity implements OnClickListener{
 
     //UI References
     private EditText fromDateEtxt;
-    private EditText txtTime,duracionToma;
+    private EditText txtTime,txtduracionToma;
     private TextView textViewDuracion;
     private Spinner spinnerDuracion;
     private Button AddMedicines;
@@ -37,28 +41,30 @@ public class add_medicines extends AppCompatActivity implements OnClickListener 
     private int mHour, mMinute;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)  {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_medicines);
 
         //Get widgets reference from XML layout
-        duracionToma = (EditText) findViewById(R.id.edtduracionToma);
+        txtduracionToma = (EditText) findViewById(R.id.edtNumDuracion);
         textViewDuracion = (TextView) findViewById(R.id.textVliew18);
-        spinnerDuracion  = (Spinner) findViewById(R.id.spinneer3);
-        sButton = (Switch) findViewById(R.id.switch1);
+        spinnerDuracion  = (Spinner) findViewById(R.id.spDuracion);
+        sButton = (Switch) findViewById(R.id.swDuracion);
         sButton.setOnClickListener(this);
          AddMedicines = (Button) findViewById(R.id.btnAddMedicines);
         AddMedicines.setOnClickListener(this);
         dateFormatter = new SimpleDateFormat("dd-MM-yyyy", Locale.US);
-        txtTime = (EditText) findViewById(R.id.editText12);
+        txtTime = (EditText) findViewById(R.id.edtHora);
         txtTime.setOnClickListener(this);
+
+
         findViewsById();
         setDateTimeField();
 
     }
 
-    private void findViewsById() {
-        fromDateEtxt = (EditText) findViewById(R.id.editText9);
+    private void findViewsById()  {
+        fromDateEtxt = (EditText) findViewById(R.id.edtFecha);
         fromDateEtxt.setInputType(InputType.TYPE_NULL);
     }
 
@@ -77,16 +83,120 @@ public class add_medicines extends AppCompatActivity implements OnClickListener 
 
 
     }
-    public void onClic(View view) {
+private void guardarMedicina(){
+    txtduracionToma.setText("Continuo");
+    EditText Nombre;
+    EditText Descripcion;
+    EditText NumDosis;
+    Spinner Dosis;
+    EditText numToma;
+    Spinner Toma;
+    EditText NumDuracion;
+    Spinner Duracion;
+    EditText Fecha;
+    EditText Hora;
+
+    Nombre=(EditText) findViewById(R.id.EdtnomMedicamento);
+    Descripcion=(EditText) findViewById(R.id.edtDescripcion);
+    NumDosis=(EditText) findViewById(R.id.edTNumDosis);
+    Dosis=(Spinner) findViewById(R.id.spDosis);
+    numToma=(EditText) findViewById(R.id.edtNumTomas);
+    Toma=(Spinner) findViewById(R.id.spTomas);
+    NumDuracion=(EditText) findViewById(R.id.edtNumDuracion);
+    Duracion=(Spinner) findViewById(R.id.spDuracion);
+    Fecha=(EditText) findViewById(R.id.edtFecha);
+    Hora=(EditText) findViewById(R.id.edtHora);
+
+    //Bandera evidenciar algun error durante la validación de los datos
+    boolean cancel = false;
+    //Variable para contener el campo a ser enfocado
+    View focusView = null;
+
+    if (TextUtils.isEmpty(Nombre.getText().toString())) {
+        Nombre.setError(getString(R.string.error_field_required));
+        focusView = Nombre;
+        cancel = true;
+    }
+    if (TextUtils.isEmpty(Descripcion.getText().toString())) {
+        Descripcion.setError(getString(R.string.error_field_required));
+        focusView = Descripcion;
+        cancel = true;
+    }
+    if (TextUtils.isEmpty(NumDosis.getText().toString())) {
+        NumDosis.setError(getString(R.string.error_field_required));
+        focusView = NumDosis;
+        cancel = true;
+    }
+    if (TextUtils.isEmpty(Dosis.getSelectedItem().toString())) {
+        // Dosis.setError(getString(R.string.error_field_required));
+        focusView = Dosis;
+        cancel = true;
+    }
+    if (TextUtils.isEmpty(numToma.getText().toString())) {
+        numToma.setError(getString(R.string.error_field_required));
+        focusView = numToma;
+        cancel = true;
+    }
+    if (TextUtils.isEmpty(Toma.getSelectedItem().toString())) {
+        //   Toma.setError(getString(R.string.error_field_required));
+        focusView = Toma;
+        cancel = true;
+    }
+    if (TextUtils.isEmpty(NumDuracion.getText().toString())) {
+        NumDuracion.setError(getString(R.string.error_field_required));
+        focusView = NumDuracion;
+        cancel = true;
+    }
+    if (TextUtils.isEmpty(Duracion.getSelectedItem().toString())) {
+        //Duracion.setError(getString(R.string.error_field_required));
+        focusView = Duracion;
+        cancel = true;
+    }
+    if (TextUtils.isEmpty(Fecha.getText().toString())) {
+        Fecha.setError(getString(R.string.error_field_required));
+        focusView = Fecha;
+        cancel = true;
+    }
+    if (TextUtils.isEmpty(Hora.getText().toString())) {
+        Hora.setError(getString(R.string.error_field_required));
+        focusView = Hora;
+        cancel = true;
+    }
+    if (cancel) {
+        //Enfocar el Campo del Error
+        focusView = Nombre;
+        focusView.requestFocus();
+
+    } else {
+
+        Medicamento medicamento=new Medicamento();
+
+        medicamento.setNombre(Nombre.getText().toString());
+        medicamento.setDescripcion(Descripcion.getText().toString());
+        medicamento.setNumeroDosis(Integer.parseInt(NumDosis.getText().toString()));
+        medicamento.setTipoDosis(Dosis.getSelectedItem().toString());
+        medicamento.setPeriodoToma(Integer.parseInt(numToma.getText().toString()));
+        medicamento.setTipoPeriodoToma(Toma.getSelectedItem().toString());
+        medicamento.setDuracionToma(Integer.parseInt(NumDuracion.getText().toString()));
+        medicamento.setTipoDuracion(Duracion.getSelectedItem().toString());
+        medicamento.setHoraInicio(Hora.getText().toString());
+        medicamento.setFechaInicio(Fecha.getText().toString());
+        medicamento.setIdMedicamento(4);
+        medicamento.setIdTratamiento(4);
+        medicamento.setSwActivo("A");
+        medicamento.setSwFinalizado("N");
+
+        ArrayList<Medicamento> medicamentos = new ArrayList<Medicamento>();
+        medicamentos.add(medicamento);
 
     }
+
+}
     @Override
-    public void onClick(View view) {
+    public void onClick  (View view) {
         //Get reference of TextView from XML layout
 
-
-
-        if (view == fromDateEtxt) {
+        if (view ==fromDateEtxt) {
             fromDatePickerDialog.show();
         } else if (view == txtTime) {
             // Get Current Time
@@ -102,7 +212,6 @@ public class add_medicines extends AppCompatActivity implements OnClickListener 
                         @Override
                         public void onTimeSet(TimePicker view, int hourOfDay,
                                               int minute) {
-
                             txtTime.setText(hourOfDay + ":" + minute);
                         }
                     }, mHour, mMinute, false);
@@ -111,22 +220,20 @@ public class add_medicines extends AppCompatActivity implements OnClickListener 
 
         if(sButton.isChecked()){
             //Do something when switch is on
-            duracionToma.setVisibility(View.INVISIBLE);
-            duracionToma.setText("Continuo");
+            txtduracionToma.setVisibility(View.INVISIBLE);
+            txtduracionToma.setText("Continuo");
             textViewDuracion.setVisibility(View.INVISIBLE);
             spinnerDuracion.setVisibility(View.INVISIBLE);
 
         }else{
             //Do something when switch is off
-            duracionToma.setVisibility(View.VISIBLE);
-            duracionToma.setText(" ");
+            txtduracionToma.setVisibility(View.VISIBLE);
+            txtduracionToma.setText(" ");
 
             textViewDuracion.setVisibility(View.VISIBLE);
             spinnerDuracion.setVisibility(View.VISIBLE);
         }
-
-
-        }
+    }
 
 
 }
